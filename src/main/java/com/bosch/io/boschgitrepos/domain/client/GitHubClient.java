da4b0426f4327d10d204cfb69990f13da6c54782
penzata <penzata86@yahoo.com>
2022-11-08 14:54:34 +0200

    commit - adding request param for pages & looping through all of them

--- src/main/java/com/bosch/io/boschgitrepos/domain/client/GitHubClient.java
+++ src/main/java/com/bosch/io/boschgitrepos/domain/client/GitHubClient.java
@@ -3,11 +3,13 @@ package com.bosch.io.boschgitrepos.domain.client;
 import com.bosch.io.boschgitrepos.domain.client.dto.RepositoryDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import java.util.List;
 
 @FeignClient(name = "gitHubClient", url = "${client.post.baseUrl}")
 public interface GitHubClient {
 
-    @GetMapping("/repos")
-    List<RepositoryDTO> getAllPublicRepos();
+    @GetMapping("/orgs/bosch-io/repos")
+    List<RepositoryDTO> getAllPublicRepos(@RequestParam("page") Integer page);
 }
--- src/main/java/com/bosch/io/boschgitrepos/domain/initilizer/BoshRepoInitializer.java
+++ src/main/java/com/bosch/io/boschgitrepos/domain/initilizer/BoshRepoInitializer.java
@@ -1,24 +1,41 @@
 package com.bosch.io.boschgitrepos.domain.initilizer;
 
 import com.bosch.io.boschgitrepos.domain.client.GitHubClient;
+import com.bosch.io.boschgitrepos.domain.client.dto.RepositoryDTO;
+import com.bosch.io.boschgitrepos.domain.model.BoschGitRepo;
+import com.bosch.io.boschgitrepos.domain.service.BoschGitRepoService;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class BoshRepoInitializer implements InitializingBean {
 
     private final GitHubClient gitHubClient;
+    private final BoschGitRepoService boschGitRepoService;
 
-    public BoshRepoInitializer(GitHubClient gitHubClient) {
+    public BoshRepoInitializer(GitHubClient gitHubClient, BoschGitRepoService boschGitRepoService) {
         this.gitHubClient = gitHubClient;
+        this.boschGitRepoService = boschGitRepoService;
     }
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        //todo extract this into constant
-        //todo add while loop to get all pages
-        gitHubClient.getAllPublicRepos()
-                        .forEach(System.out::println);
 
+        int pageSize = 1;
+        int page = 1;
+
+        while (pageSize > 0) {
+            List<BoschGitRepo> allRepos = gitHubClient.getAllPublicRepos(page).stream()
+                    .map(RepositoryDTO::toModel).toList();
+
+            pageSize = allRepos.size();
+
+            if (pageSize > 0) {
+                boschGitRepoService.saveBoshGitRepos(allRepos);
+            }
+            page++;
+        }
     }
 }
