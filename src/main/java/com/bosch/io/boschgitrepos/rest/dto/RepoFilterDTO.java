5a6bba80d6430a8e3a0460dfd49c9de6de817fff
penzata <penzata86@yahoo.com>
2022-11-08 16:35:31 +0200

    commit - minimizing url params

--- src/main/java/com/bosch/io/boschgitrepos/rest/dto/RepoFilterDTO.java
+++ src/main/java/com/bosch/io/boschgitrepos/rest/dto/RepoFilterDTO.java
@@ -3,14 +3,14 @@ package com.bosch.io.boschgitrepos.rest.dto;
 import com.bosch.io.boschgitrepos.domain.model.BoschGitRepo;
 
 public record RepoFilterDTO(String name,
-                            String description,
-                            String language) {
+                            String desc,
+                            String lang) {
 
     public BoschGitRepo toExample() {
         BoschGitRepo boschGitRepo = new BoschGitRepo();
         boschGitRepo.setFullName(name);
-        boschGitRepo.setDescription(description);
-        boschGitRepo.setProgrammingLanguage(language);
+        boschGitRepo.setDescription(desc);
+        boschGitRepo.setProgrammingLanguage(lang);
 
         return boschGitRepo;
     }
