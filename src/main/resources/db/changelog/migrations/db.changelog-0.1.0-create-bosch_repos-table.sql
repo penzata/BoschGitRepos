03af0b87b80d210155554a27e6f861f90a8eff34
penzata <penzata86@yahoo.com>
2022-11-08 15:17:06 +0200

    commit - fixing table

--- src/main/resources/db/changelog/migrations/db.changelog-0.1.0-create-bosch_repos-table.sql
+++ src/main/resources/db/changelog/migrations/db.changelog-0.1.0-create-bosch_repos-table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE bosch_repos
 (
     id SERIAL PRIMARY KEY NOT NULL,
-    fullName VARCHAR(50) NOT NULL,
-    description VARCHAR(256) NOT NULL,
-    programmingLanguage VARCHAR(50) NOT NULL,
-    htmlUrl VARCHAR(256) NOT NULL
+    full_name VARCHAR(256) NOT NULL,
+    description CHARACTER LARGE OBJECT,
+    programming_language VARCHAR(50),
+    html_url VARCHAR(256) NOT NULL
 )
